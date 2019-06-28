26e7569b71ef8e0849937c9cb31f310907f4cdd5
polarag <23058078+polarag@users.noreply.github.com>
2019-06-28 21:35:07 +0200

    Add printing bet degree closeness

--- drawtool.py
+++ drawtool.py
@@ -18,5 +18,10 @@ pos = nx.spring_layout(G)
 nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels)
 d = nx.betweenness_centrality(G, weight="weight", normalized=False)
 
+print(nx.betweenness_centrality(G, weight="weight", normalized=False))
+print(nx.closeness_centrality(G, weight="weight", normalized=False))
+print(nx.degree_centrality(G, weight="weight", normalized=False))
+
+
 nx.draw(G,pos, node_size=[max(v * 1500,100) for v in d.values()] , with_labels=True)
 plt.show()
