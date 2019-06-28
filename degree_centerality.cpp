c32f2f5b24a271b807d03a49a5512f5d8d67c367
Giovanni Mounir <giovannimounir@gmail.com>
2019-06-28 20:05:54 +0200

    Separate degree centerality from source.cpp

---
+++ degree_centerality.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n,m,temp;
+	cin >> n >> m;
+	int * _array = new int[n];
+	for (int i = 0; i < n; i++)
+		_array[i] = 0;
+	for (int i = 0; i < m; i++)
+	{
+		cin >> temp;
+		_array[temp]++;
+		cin >> temp;
+		_array[temp]++;
+		cin >> temp;
+	}
+
+	for (int i = 0; i < n; i++)
+		cout << _array[i] << "\n";
+}
